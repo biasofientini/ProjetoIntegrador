61798dd64a348ce4eef67233daa64d510836fb72
Adilson Porto <juportoleal@gmail.com>
2021-05-31 19:14:02 -0300

    Adicionando model Usuario

--- lifeshop/src/main/java/com/ecommerce/lifeshop/model/Usuario.java
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/model/Usuario.java
@@ -1,5 +1,88 @@
 package com.ecommerce.lifeshop.model;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size (min = 5, max = 45, message = "Erro tamanho nome usuário")
+	private String nome;
+	
+	@Size ()
+	private String email;
+	
+	@Size (min = 8, message = "Erro tamanho senha usuário")
+	private String senha;
+	
+	@NotNull
+	private String endereco;
+	
+	@Size(min = 8, max= 8, message = "Erro tamanho cep usuário")
+	private String cep;
+
+	//Id
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	//Nome
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	//Email
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	//Senha
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	//Endereço
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	//CEP
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 }
