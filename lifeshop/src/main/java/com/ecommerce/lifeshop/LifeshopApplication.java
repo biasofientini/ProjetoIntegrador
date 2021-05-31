54d7ff8fd013523c86f168b858539273419f479d
biasofientini <biasofientini@gmail.com>
2021-05-31 18:18:46 -0300

    LifeShop Spring Boot

---
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/LifeshopApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.lifeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeshopApplication.class, args);
+	}
+
+}
---
+++ lifeshop/src/test/java/com/ecommerce/lifeshop/LifeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.lifeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
