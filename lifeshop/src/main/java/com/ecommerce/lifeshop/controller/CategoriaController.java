b3519eef1d2b5389d74f4772c4ee0702a01fa97b
ThamiresHonorato <thamireshonorato1998@gmail.com>
2021-05-31 23:01:12 -0300

    Implementando verbos http da CategoriaController

--- lifeshop/src/main/java/com/ecommerce/lifeshop/controller/CategoriaController.java
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/controller/CategoriaController.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,18 @@ public class CategoriaController {
 		Optional<Categoria> opt = service.findById(id);
 		return ResponseEntity.status(HttpStatus.OK).body(opt.get());
 	}
-	
+	@PostMapping ("/salvar")
+	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(categoria));
+
+	}
+	@PutMapping("/atualizar")
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.OK).body(service.save(categoria));
+	}
+	@DeleteMapping ("/delete/{id}")
+	public void delete(@PathVariable Long id ) {
+	service.delete(id);
+	}
+
 }
