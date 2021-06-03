53d7e4185c16cd1b999352c296e7d849194655c6
Adilson Porto <juportoleal@gmail.com>
2021-06-03 13:55:07 -0300

    Adicionando CRUD Usuario

---
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/controller/UsuarioController.java
@@ -0,0 +1,58 @@
+package com.ecommerce.lifeshop.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.lifeshop.model.Usuario;
+import com.ecommerce.lifeshop.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping("/todos")
+	public ResponseEntity<List<Usuario>> getAllUsuario(){
+		return service.findAllUsuario();
+	}
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable Long id){
+		return service.findUsuarioById(id);
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getUsuarioByNome(@PathVariable String nome){
+		return service.findUsuarioByNome(nome);
+	}
+	
+	@Valid
+	@PostMapping("/salvar")
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
+		return ResponseEntity.status(201).body(service.save(usuario));
+	}
+	
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario){
+		return ResponseEntity.status(200).body(service.save(usuario));
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
+	}
+	
+}
---
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.ecommerce.lifeshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.lifeshop.model.Usuario;
+import com.ecommerce.lifeshop.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	public UsuarioRepository repository;
+	
+	//trazer todos
+	public ResponseEntity<List<Usuario>> findAllUsuario(){
+		List<Usuario> usuarios = repository.findAll();
+		if(!usuarios.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(usuarios);
+		}
+	}
+	//trazer por id
+	public ResponseEntity<Usuario> findUsuarioById(Long id){
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(404).build());
+	}
+	
+	//trazer usuários por nome
+	public ResponseEntity<List<Usuario>> findUsuarioByNome(String nome){
+		List<Usuario> usuarios = repository.findAllByNomeContainingIgnoreCase(nome);
+		if(!usuarios.isEmpty()) {
+			return ResponseEntity.status(302).body(usuarios);
+		} else {
+			return ResponseEntity.status(404).build();
+		}
+	}
+	//salvar usuario
+	public Usuario save(Usuario usuario) {
+		return repository.save(usuario);
+	}
+	
+	//deletar por id
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+
+}
