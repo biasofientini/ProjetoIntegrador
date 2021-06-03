0ffb7051bcbe91f3828f347e65ab730f1a8cccb2
Nilton Santos <jvictormaciel@yahoo.com.br>
2021-06-03 12:00:12 -0300

    Adicionando CRUD Produtos

---
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/repository/ProdutoRepository.java
@@ -0,0 +1,15 @@
+package com.ecommerce.lifeshop.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.ecommerce.lifeshop.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findByNomeContainingIgnoreCase(String nome);
+	public Optional<List<Produto>> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
---
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/service/ProdutoService.java
@@ -0,0 +1,45 @@
+package com.ecommerce.lifeshop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.ecommerce.lifeshop.model.Produto;
+import com.ecommerce.lifeshop.repository.ProdutoRepository;
+
+public class ProdutoService { 
+	
+	@Autowired
+	private ProdutoRepository repository;
+
+	//trazer por id
+	public ResponseEntity<Produto> findProdutoById(Long id) {
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(404).build());
+	}
+
+	//salvar categoria
+	public Produto save(Produto produto) {
+		return repository.save(produto);
+	}
+	
+	//deletar por id
+	public void delete (Long id) {
+		repository.deleteById(id);
+	}
+	
+	 //trazer por descricao
+	public ResponseEntity<List<Produto>> findProdutoByDescricao(String descricao){
+		Optional<List<Produto>> categorias = repository.findAllByDescricaoContainingIgnoreCase(descricao);
+		if(!categorias.isEmpty()) {
+			return ResponseEntity.status(302).body(categorias.get());
+		} else {
+		return ResponseEntity.status(204).build();
+		}
+	}
+	
+	
+}
