a23dedf7df6c6e1f21816735d25bc3385bbec876
ThamiresHonorato <thamireshonorato1998@gmail.com>
2021-06-04 12:33:23 -0300

    Adicionando relacionamentos entre as tabelas produto e categoria

--- lifeshop/src/main/java/com/ecommerce/lifeshop/model/Categoria.java
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/model/Categoria.java
@@ -1,12 +1,18 @@
 package com.ecommerce.lifeshop.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -24,7 +30,11 @@ public class Categoria {
 	private String descricao;
 
 	private String urlImagem;
-
+	
+	@OneToMany(mappedBy= "categoria",cascade= CascadeType.ALL )
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produtos;
+	
 	public Long getId() {
 		return id;
 	}
@@ -57,4 +67,13 @@ public class Categoria {
 		this.urlImagem = urlImagem;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
 }
