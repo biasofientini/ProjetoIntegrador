8f5686dd4953bd01c8abc93539cdfe703e438ba8
biasofientini <biasofientini@gmail.com>
2021-07-12 00:11:58 -0300

    Deploy Lifeshop

--- lifeshop/src/main/java/com/ecommerce/lifeshop/service/OrderService.java
+++ lifeshop/src/main/java/com/ecommerce/lifeshop/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.ecommerce.lifeshop.service;
 
-import java.util.List;
+import java.util.HashMap;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +16,13 @@ import com.ecommerce.lifeshop.model.CartItem;
 import com.ecommerce.lifeshop.model.Order;
 import com.ecommerce.lifeshop.model.OrderDTO;
 import com.ecommerce.lifeshop.model.OrderItem;
-import com.ecommerce.lifeshop.model.OrderItemDTO;
+import com.ecommerce.lifeshop.model.Product;
 import com.ecommerce.lifeshop.model.User;
 import com.ecommerce.lifeshop.repository.CartItemRepository;
 import com.ecommerce.lifeshop.repository.CartRepository;
 import com.ecommerce.lifeshop.repository.OrderItemRepository;
 import com.ecommerce.lifeshop.repository.OrderRepository;
+import com.ecommerce.lifeshop.repository.ProductRepository;
 import com.ecommerce.lifeshop.repository.UserRepository;
 
 @Service
@@ -39,6 +42,9 @@ public class OrderService {
 
 	@Autowired
 	private OrderItemRepository repositoryOrderItem;
+
+	@Autowired
+	private ProductRepository repositoryProduct;
 
 	public ResponseEntity<List<OrderDTO>> getAllByUser(String token) {
 		Optional<User> user = repositoryUser.findByToken(token);
@@ -87,20 +93,37 @@ public class OrderService {
 			order.setStatus("Em separação");
 			repository.save(order);
 			List<CartItem> cartitems = repositoryCartItem.findByCart(cart.get());
-			Float finalPrice = 0.0f;
-			for (int i = 0; i < cartitems.size(); i++) {
-				OrderItem orderitem = new OrderItem();
-				orderitem.setOrder(order);
-				orderitem.setQuantity(cartitems.get(i).getProductQty());
-				orderitem.setPurchasedProduct(cartitems.get(i).getProduct());
-				orderitem.setUnitPrice(cartitems.get(i).getProduct().getPrice());
-				finalPrice += orderitem.getUnitPrice() * orderitem.getQuantity();
-				repositoryOrderItem.save(orderitem);
+			HashMap<Long, Integer> counterProducts = groupByProduct(cartitems);
+
+			if(checkStockProducts(counterProducts)){
+				Float finalPrice = 0.0f;
+				for(Map.Entry<Long, Integer> e: counterProducts.entrySet()){
+					Long productId = e.getKey();
+					Integer productQty = e.getValue();
+					Optional<Product> product = repositoryProduct.findById(productId);
+					OrderItem orderitem = new OrderItem();
+					orderitem.setOrder(order);
+					orderitem.setQuantity(productQty);
+
+					orderitem.setPurchasedProduct(product.get());
+					product.get().setStock(product.get().getStock() - productQty);
+
+					orderitem.setUnitPrice(product.get().getPrice());
+					finalPrice += productQty * product.get().getPrice();
+
+					repositoryOrderItem.save(orderitem);
+				}
+				Float discount = Math.min(finalPrice, user.get().getPoints()/10);
+				int usedPoints = discount.intValue() * 10;
+				user.get().setPoints(user.get().getPoints() + finalPrice.intValue() - usedPoints);
+				finalPrice -= discount;
+				OrderDTO orderdto = OrderDTO.convert(repository.save(order));
+				orderdto.finalPrice = finalPrice;
+				return ResponseEntity.ok().body(orderdto);
 			}
-			user.get().setPoints(user.get().getPoints() + finalPrice.intValue());
-			OrderDTO orderdto = OrderDTO.convert(repository.save(order));
-			orderdto.finalPrice = finalPrice;
-			return ResponseEntity.ok().body(orderdto);
+			else{
+				return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).build();
+			}
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	}
@@ -116,4 +139,26 @@ public class OrderService {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	}
 
+	private HashMap<Long, Integer> groupByProduct(List<CartItem> cartitems){
+		HashMap<Long, Integer> counter = new HashMap<>();
+		for(CartItem cartItem: cartitems){
+			Integer qty = cartItem.getProductQty();
+			Long productId = cartItem.getProduct().getId();
+			Integer newQty = counter.getOrDefault(productId, 0) + qty;
+			counter.put(productId, newQty);
+		}
+		return counter;
+	}
+
+	private boolean checkStockProducts(HashMap<Long, Integer> counterProducts){
+		for(Map.Entry<Long, Integer> e: counterProducts.entrySet()){
+			Long productId = e.getKey();
+			Optional<Product> product = repositoryProduct.findById(productId);
+			boolean canBuy = product.isPresent() && product.get().getStock() >= e.getValue();
+			if(!canBuy){
+				return false;
+			}
+		}
+		return true;
+	}
 }
